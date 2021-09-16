510791137198c88f1a1a8201b28200a4cff28320
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-09-16 21:34:04 +0200

    example 2

--- examples/arrest_example_2.rs
+++ examples/arrest_example_2.rs
@@ -10,20 +10,38 @@ async fn main() {
     let mut client = arrest::Client::new(String::from(""), String::from(""));
     client.set_headers();
     client.set_client();
+    //
     let urls: Vec<String> = vec![
         String::from("http://httpbin.org/anything"),
         String::from("http://httpbin.org/anything"),
     ];
-    let httpbin = HttpBinAnything::default();
-    let res = client.arrest(urls, httpbin).await.unwrap();
+
+    let mut httpbin_second = HttpBinAnything::default();
+    httpbin_second.update_queue(String::from("http://httpbin.org/anything"));
+    httpbin_second.update_queue(String::from("http://httpbin.org/anything"));
+    let res = httpbin_second.get_queue(client).await;
     dbg!(res);
 }
 
+impl HttpBinAnything {
+    pub async fn get_queue(self, client: arrest::Client) -> Vec<HttpBinAnything> {
+        let res = client.arrest(self.flush_queue(), self).await.unwrap();
+        res
+    }
+    pub fn update_queue(&mut self, url: String) {
+        self.queue.push(url);
+    }
+    pub fn flush_queue(&self) -> Vec<String> {
+        self.queue.clone()
+    }
+}
 //http://httpbin.org/anything
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct HttpBinAnything {
     method: String,
     url: String,
+    #[serde(skip)]
+    queue: Vec<String>,
 }
 
 impl Default for HttpBinAnything {
@@ -31,6 +49,7 @@ impl Default for HttpBinAnything {
         HttpBinAnything {
             method: String::from(""),
             url: String::from(""),
+            queue: vec![],
         }
     }
 }
